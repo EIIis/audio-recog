c31d433a94aaa907b70bfe5346b2fb2b252328fe
Ellis Alcantara <ellisalcantara@gmail.com>
2021-04-27 16:59:12 -0500

    Main source code and README update

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-# audio-recog
+# Audio Recoginiton Web Application #  
+   
+## Dependecies to run the it yourself ##
 
-## Install flask  
-python3 -m pip install flask  
+|Framework/API     |PIP Commands                                             
+|------------------|-------------------------------
+|Flask             |`python3 -m pip install flask`            
+|SpeechRecognition |`python3 -m pip install SpeechRecognition` 
 
-## Install SpeechRecognition
-python3 -m pip install SpeechRecognition
+## Running on your local enviorment ##
 
-## Run on local enviorment  
-python3 app.py  
-then..
-localhost:5000
+1. Go to directory containing app.py through terminal
+2. Type the following command `python3 app.py`
+3. Go to your browser and in the url search fo `localhost:5000`  
+
+## Current Issues ##
+* ***Only Takes In .wav Files!***
+* Homepage needs to be reworked to beautify it a tad bit
+* Large audio files are ***NOT*** supported. Will fail to do anything  
+  
+* Any other problems? Feel to do a pull request and/or email me at ellisalcantara@gmail.com
--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
+    transcript = ""
     if request.method == "POST":
-        print("Data received")
-    return render_template('index.html')
+        print("Success!")
+        if "file" not in request.files: # Checks if there is a file, if not it redirects back to the homepage
+            return redirect(request.url)
+
+        file = request.files["file"]
+        
+        if file.filename == "": # Checks to make sure the file has a name, if not it redirects back to the homepage
+            return redirect(request.url)
+
+        if file: # If file is valid, we analyze it with speech recogniztion and have the transcript
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as sourceFile:
+                data = recognizer.record(sourceFile)
+            transcript = recognizer.recognize_google(data, key = None)
+
+    return render_template('index.html', transcript = transcript)
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
